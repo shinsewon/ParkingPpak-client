17105971ae6f7efe65a3bc4655a2ce42cdfcb9e0
hyewon do <tsc03018@gmail.com>
2022-03-14 21:50:19 +0900

    style: 로그인 페이지 스타일

--- src/screens/Auth/index.tsx
+++ src/screens/Auth/index.tsx
@@ -14,7 +14,7 @@ const Stack = createNativeStackNavigator<AuthStackParams>();
 
 export default function AuthStack() {
   return (
-    <Stack.Navigator>
+    <Stack.Navigator screenOptions={{headerShown: false}}>
       <Stack.Screen name="Login" component={LoginScreen} />
       <Stack.Screen name="Register" component={RegisterScreen} />
     </Stack.Navigator>
