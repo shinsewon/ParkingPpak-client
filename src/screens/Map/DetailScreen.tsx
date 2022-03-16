3400a3cdfc06fb0b80892aa7cafb632e2f2a9bea
sewon <shin6403@gmail.com>
2022-03-17 00:05:32 +0900

    wip

--- src/screens/Map/DetailScreen.tsx
+++ src/screens/Map/DetailScreen.tsx
@@ -4,7 +4,7 @@ import {StyleSheet, Text, View} from 'react-native';
 export default function DetailScreen() {
   return (
     <View style={styles.block}>
-      <Text>DetailScreen1</Text>
+      <Text>DetailScreen12</Text>
     </View>
   );
 }
